4b0aae2250d62a39b1d9c91a3dcdac7f4e072d2c
Ermina_Joseph <ermina.joseph@gmail.com>
2020-03-22 08:51:46 -0400

    Updates

---
+++ precache-manifest.f78dbe244d4ec2007496d9496251978c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddaacbc68fbfee46cd19812f2fe7de3e",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "785ccd11680a6d455700",
+    "url": "/clickygame/static/css/main.278872fd.chunk.css"
+  },
+  {
+    "revision": "fbf7edc898850ace7664",
+    "url": "/clickygame/static/js/2.0f7e4126.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clickygame/static/js/2.0f7e4126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785ccd11680a6d455700",
+    "url": "/clickygame/static/js/main.efc3e9b2.chunk.js"
+  },
+  {
+    "revision": "dac7d6f2cc6e66ab07d5",
+    "url": "/clickygame/static/js/runtime-main.e65163b8.js"
+  }
+]);
